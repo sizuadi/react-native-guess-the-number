cab6343a2e0787152da2736ed829f16687624fb2
Adi Siswanto <adi15siswanto@gmail.com>
2025-01-09 22:11:32 +0700

    update styling

--- components/pages/StartGamePage.tsx
+++ components/pages/StartGamePage.tsx
@@ -1,14 +1,47 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { LinkButton } from '../atoms/LinkButton';
 
 export default function StartGamePage() {
 	return (
 		<>
-			<View>
-				<Text>Hello World!</Text>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.inputText}
+					maxLength={2}
+					keyboardType='number-pad'
+					autoCapitalize='none'
+					autoCorrect={false}
+				/>
 				<LinkButton link='/in_game' text='In Game' />
 				<LinkButton link='/in_game' text='In Game' />
 			</View>
 		</>
 	);
 }
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		marginTop: 100,
+		backgroundColor: '#0b6599',
+		padding: 20,
+		marginHorizontal: 25,
+		borderRadius: 15,
+		elevation: 10,
+		shadowColor: 'black',
+		shadowOpacity: 0.26,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 10,
+		alignItems: 'center',
+	},
+	inputText: {
+		color: '#fff',
+		borderBottomColor: '#fff',
+		borderBottomWidth: 2,
+		width: 50,
+		paddingBottom: 5,
+		fontSize: 24,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		marginBottom: 10,
+	},
+});
