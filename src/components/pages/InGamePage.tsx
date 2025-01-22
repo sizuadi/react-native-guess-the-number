49d275ecf6038b0887648c082ca33310849da57d
Adi Siswanto <adi15siswanto@gmail.com>
2025-01-22 18:33:44 +0700

    fix: in game logic

--- src/components/pages/InGamePage.tsx
+++ src/components/pages/InGamePage.tsx
@@ -1,7 +1,9 @@
+import { DefaultButton } from '@/components/atoms/DefaultButton';
 import NumberContainer from '@/components/atoms/NumberContainer';
 import Title from '@/components/atoms/Title';
+import Colors from '@/constants/colors';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 interface inGameProps {
 	userNumber: number;
@@ -14,17 +16,46 @@ function generateRandomBetween(min: number, max: number, exclude: number) {
 	}
 	return randomNumber;
 }
+
+let maxBoundary = 100;
+let minBoundary = 1;
 export default function InGamePage({ userNumber }: inGameProps) {
-	const initialGuess = generateRandomBetween(1, 100, userNumber);
+	const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
 	const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+	function nextGuessHandler(direction: 'lower' | 'greater') {
+		if (
+			(direction === 'lower' && currentGuess < userNumber) ||
+			(direction === 'greater' && currentGuess > userNumber)
+		) {
+			Alert.alert("Don't lie!", 'You know that this is wrong...', [
+				{ text: 'Sorry!', style: 'destructive' },
+			]);
+			return;
+		}
+		if (direction === 'lower') {
+			maxBoundary = currentGuess;
+		} else {
+			maxBoundary = currentGuess + 1;
+		}
+
+		// TODO : GAME OVER PAGE
+		// if (condition) {
+		// }
+		const newGuessNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+		setCurrentGuess(newGuessNumber);
+	}
 	return (
 		<View style={styles.screen}>
 			<Title>Opponent's Guess</Title>
 			{/* GUESS */}
 			<NumberContainer>{currentGuess}</NumberContainer>
-			<View>
-				<Text>Higher or lower?</Text>
-				{/* - + */}
+			<View style={styles.controlContainer}>
+				<Text style={styles.controlText}>Higher or lower?</Text>
+				<View style={styles.buttonContainer}>
+					<DefaultButton onPressButton={() => nextGuessHandler('lower')} text='-' />
+					<DefaultButton onPressButton={() => nextGuessHandler('greater')} text='+' />
+				</View>
 			</View>
 			{/* LOG ROUNDS */}
 		</View>
@@ -35,5 +66,23 @@ const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
 		padding: 24,
+	},
+	controlContainer: {
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 16,
+	},
+	controlText: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 16,
+		color: Colors.white,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		width: 300,
+		maxWidth: '80%',
 	},
 });
